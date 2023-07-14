a727b6f4ec5ea3627f6adb796daf2db126f16ad2
Jasper Hjorth <jasper.hjorth@gmail.com>
2023-07-15 01:32:36 +0300

    Add simple Django login

---
+++ webapp/pages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.template import loader
+from django.db import transaction 
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def homePageView(request):
+	return render(request, 'pages/index.html')
+ 
